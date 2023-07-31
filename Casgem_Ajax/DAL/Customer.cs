f7b8db8f10d55e5887c1034828c2d5d544bf860c
aleyna çelik <celikaleyna71@gmail.com>
2023-07-31 12:17:44 +0300

    Add project files.

---
+++ Casgem_Ajax/DAL/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace Casgem_Ajax.DAL
+{
+    public class Customer
+    {
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string City { get; set; }
+       
+    }
+}
